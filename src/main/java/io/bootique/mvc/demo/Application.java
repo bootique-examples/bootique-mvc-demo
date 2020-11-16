8e25396b39123219c21427b2daa809688da422d1
andreykravchenko <andrey.kravchenko@objectstyle.com>
2020-11-16 13:27:49 +0300

    Changed version to 2.0

--- src/main/java/io/bootique/mvc/demo/Application.java
+++ src/main/java/io/bootique/mvc/demo/Application.java
@@ -1,13 +1,13 @@
 package io.bootique.mvc.demo;
 
-import com.google.inject.Binder;
-import com.google.inject.Module;
+import io.bootique.BaseModule;
 import io.bootique.Bootique;
+import io.bootique.di.Binder;
 import io.bootique.jersey.JerseyModule;
 import io.bootique.jetty.JettyModule;
 import io.bootique.mvc.demo.contoller.BootiqueController;
 
-public class Application implements Module {
+public class Application extends BaseModule {
 
     public static void main(String[] args) {
         Bootique
